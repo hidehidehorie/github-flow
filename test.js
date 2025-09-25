67b3f56d7f5af1fec74b2c0283a2a79b5bb0262d
hidehidehorie <54232377+hidehidehorie@users.noreply.github.com>
2025-09-25 16:13:01 +0900

    add console5 (#5)

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-console.log("test")
-console.log("feat1")
-console.log("feat2")
-console.log("feat3")
+console.log("test");
+console.log("feat1");
+console.log("feat2");
+console.log("feat3");
+console.log("feat5");
